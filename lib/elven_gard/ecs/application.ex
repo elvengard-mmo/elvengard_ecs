83822e4394d70f8e05ad1c84a9a8b20e2ae5004e
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-09-17 19:06:37 +0200

    :fire: Remove old nnesia ClusterManager

--- lib/elven_gard/ecs/application.ex
+++ lib/elven_gard/ecs/application.ex
@@ -7,12 +7,7 @@ defmodule ElvenGard.ECS.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      # FIXME: Hadcoded for now
-      {ElvenGard.ECS.MnesiaBackend.ClusterManager, [retry: true]},
-      {Config.backend(), []}
-    ]
-
+    children = [{Config.backend(), []}]
     opts = [strategy: :one_for_one, name: ElvenGard.ECS.Supervisor]
     Supervisor.start_link(children, opts)
   end
