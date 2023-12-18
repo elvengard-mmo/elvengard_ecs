cdf202b17f1694831ebf96501073e3ac03dbae0c
DarkyZ aka NotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-12-18 16:51:16 +0100

    :ambulance: Fix span returns

--- lib/elven_gard/ecs/topology/partition.ex
+++ lib/elven_gard/ecs/topology/partition.ex
@@ -88,7 +88,7 @@ defmodule ElvenGard.ECS.Topology.Partition do
       :telemetry.span(
         [:elvengard_ecs, :startup_system_run],
         metadata,
-        fn -> module.run(context) end
+        fn -> {module.run(context), _metadata = %{}} end
       )
     end)
 
@@ -220,7 +220,7 @@ defmodule ElvenGard.ECS.Topology.Partition do
     :telemetry.span(
       [:elvengard_ecs, :system_run],
       metadata,
-      fn -> system.run(event, context) end
+      fn -> {system.run(event, context), _metadata = %{}} end
     )
 
     value
@@ -240,7 +240,7 @@ defmodule ElvenGard.ECS.Topology.Partition do
     :telemetry.span(
       [:elvengard_ecs, :system_run],
       metadata,
-      fn -> system.run(context) end
+      fn -> {system.run(context), _metadata = %{}} end
     )
 
     system
